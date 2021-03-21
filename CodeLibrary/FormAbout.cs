8610d80c3bc5f440455636a09d489daaae44c83d
AV <av.github@gmail.com>
2021-03-21 09:18:49 +0100

    Fixed spelling error in release note

--- CodeLibrary/FormAbout.cs
+++ CodeLibrary/FormAbout.cs
@@ -11,7 +11,7 @@ namespace CodeLibrary
         private readonly string _Content = @"
 ----------------------------------------------------------------------
 Version 2.x:
--   Improved TreeView behavior: changed behavior of the short cut keys - the now respond to KeyDown instead of the KeyUp event for better response
+-   Improved TreeView behavior: changed behavior of the short cut keys - they now respond to KeyDown instead of the KeyUp event for better response
 
 Version 2.2:
 -   Added Setting Screen
